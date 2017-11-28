985a891ecf1d0e0c59be047d829e4f326a5f83b7
mark0092 <Berezovik.MYu@phystech.edu>
2017-11-28 19:16:40 +0300

    КЛЮЧ ГОТОВ

--- bitwise.c
+++ bitwise.c
@@ -11,6 +11,20 @@ char bit_search(char *a, int m) { //возвращает значение бит
 	return ((1 << 7 - (m % 8))  &  a[(m / 8)]) >> 7 - (m % 8);
 }
 
-//void cyclic_shift_right(char *a, int m) { //сдвигает на m бит вправо
-//
-//}
+void cyclic_shift_right(char *a, int m) { //сдвигает на m бит вправо
+	int i, j;
+	char b[4];
+	for (j = 0; j < m; j++){
+
+		for (i = 0; i < 4; i++)
+			b[i] = bit_search(a, i * 8 + 7);
+		
+		for (i = 0; i < 4; i++)
+			a[i] = a[i] >> 1;
+		
+		bit_add (a, 0, b[3]);
+
+		for (i = 0; i < 3; i++)
+			bit_add(a, (i + 1) * 8, b[i]);
+	}
+}
--- keygen.c
+++ keygen.c
@@ -8,6 +8,7 @@ char *key_change(char *key_null) { // на вход поступает 56-бит
 	char *key_null_new = malloc(8 * sizeof(char));
 	int i, j, num;
 	char data;
+	
 	for(i = 0; i < 8; i++) {
 		num = 0;
 		for(j = 0; j < 7; j++) {
@@ -21,30 +22,54 @@ char *key_change(char *key_null) { // на вход поступает 56-бит
 		else 
 			bit_add(key_null_new, i * 8 + 7, 0);
 	}
+	
 	char *key_null_change = malloc(8 * sizeof(char));
+	
 	for (i = 0; i < 64; i++) {
 		if(i % 8 != 7)
 			bit_add(key_null_change, i, bit_search(key_null_new, map[i - (i/8)] - 1));
 		else
 			bit_add(key_null_change, i, bit_search(key_null_new, i));
 	}
+
 	free(key_null_new);
+	
 	return key_null_change;
 }
 
 char **keys_create(char *key_null) { //создается 16 ключей, для 16 циклов
-	//int map[48]={14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
+	int map[48] = {14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
+	int move[16] = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
 	char **keys = malloc(16 * sizeof(char *));
-	int i;
+	int i, j;
+
 	char *key_null_new = key_change(key_null);
 	char *C = malloc(8 * sizeof(char)), *D = malloc(8 * sizeof(char));
+
 	for (i = 0; i < 32; i++) {
 		if(i % 8 != 7)
 			bit_add(C, i - (i/8), bit_search(key_null_new, i));	
 	}
+
 	for (i = 32; i < 64; i++) {
 		if(i % 8 != 7)
 			bit_add(D, i - (i/8) - 28, bit_search(key_null_new, i));	
+	}
+
+	for (j = 0; j < 28; j++)
+			printf("%d ", bit_search(D, j));
+	printf("\n");
+
+	for (i = 0; i < 16; i++) {
+		cyclic_shift_right(C, move[i]);
+		cyclic_shift_right(D, move[i]);
+		keys[i] = malloc(6 * sizeof(char));
+		for (j = 0; j < 48; j++) {
+			if (map[j] > 32)
+				bit_add(keys[i], j, bit_search(D, map[j] % 32 - 1));
+			else
+				bit_add(keys[i], j, bit_search(C, map[j] - 1));
+		}
 	}
 	return keys;
 }
