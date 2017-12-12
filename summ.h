85a72dd59e5a5f81064f12335e2144366c4e69a6
mark0092 <Berezovik.MYu@phystech.edu>
2017-12-12 12:50:14 +0300

    summ.h

---
+++ summ.h
@@ -0,0 +1,14 @@
+#ifndef CIPHER_SUMM_H
+#define CIPHER_SUMM_H
+
+void bit_add(char *, int, char);
+
+char bit_search(char *, int);
+
+void bit_change(char** , char** , int);
+
+char* expansion(char**);
+
+void blocks_break(char *, int);
+
+#endif
